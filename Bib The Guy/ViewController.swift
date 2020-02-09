46af28f5bca7ea04931b11df14bd196c77f50ae2
yeindaw <yedawin@gmail.com>
2020-02-09 16:17:37 -0500

    4.3

--- Bib The Guy/ViewController.swift
+++ Bib The Guy/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var imageToPunch: UIImageView!
+    
+    var audioPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +24,20 @@ class ViewController: UIViewController {
          self.imageToPunch.bounds = CGRect(x: self.imageToPunch.bounds.origin.x + shrinkValue, y: self.imageToPunch.bounds.origin.y + shrinkValue, width: self.imageToPunch.bounds.size.width - shrinkValue, height: self.imageToPunch.bounds.size.height - shrinkValue)
          UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: { self.imageToPunch.bounds = bounds}, completion: nil)
      }
-     
+    
+    func playSound(name: String){
+        if let sound = NSDataAsset(name: name){
+            do{
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            }catch{
+                print("😡 ERROR: \(error.localizedDescription) Could not initialize AVAudioPlayer")
+            }
+        } else {
+            print("😡 ERROR: Could not read data from file sound0 😡")
+        }
+    }
+    
     @IBAction func libraryPressed(_ sender: UIButton) {
     }
     
@@ -29,6 +45,7 @@ class ViewController: UIViewController {
     }
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
         animateImage()
+        playSound(name: "punchSound")
     }
 }
 
